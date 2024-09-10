2956441f631da4f726586666fff20fff6b2d4f18
A <aytacacar@icloud.com>
2024-09-10 14:09:48 +0200

    Initial Commit

---
+++ SimpleToDoList/SimpleToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoListApp.swift
+//  SimpleToDoList
+//
+//  Created by Privat on 10.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
